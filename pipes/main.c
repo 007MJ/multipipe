6be264bcba1eeb72010524d8aa67ef4d0edfc248
007MJ <k.mijiza@gmail.com>
2024-02-14 21:57:13 -0500

    parsing

--- pipes/main.c
+++ pipes/main.c
@@ -17,15 +17,15 @@ int main(int ac, char **av, char **envp)
     if (ac == 1)
     {
         char *one[] = {"ls", "-l", NULL};
-        char *two[] = {"wc", NULL};
-        char *three[] = {"wc", NULL};
-        char *four[] = {"ls -l", NULL};
+        // char *two[] = {"wc", NULL};
+        // char *three[] = {"wc", NULL};
+        // char *four[] = {"ls -l", NULL};
         if (!head)
             return (printf("head did't malloc"), -1);
         append(head, 0, pipes, one, "ls", nb_pipe);
-        append(head, 1, pipes, two, "wc", nb_pipe);
-        append(head, 2, pipes, three, "wc", nb_pipe);
-        append(head, 3, pipes, four, "ls", nb_pipe);
+        // append(head, 1, pipes, two, "wc", nb_pipe);
+        // append(head, 2, pipes, three, "wc", nb_pipe);
+        // append(head, 3, pipes, four, "ls", nb_pipe);
         run_command(envp, head);
     }
     close_pipes(*head, (*head)->nb_pipes);
