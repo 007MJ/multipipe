73c195cb1798475befadd00cbf3c090499ffd35c
007MJ <k.mijiza@gmail.com>
2024-02-29 18:32:19 -0500

    expan_pars

---
+++ bankai/parsing/expan_pars/make_cmd.c
@@ -0,0 +1,14 @@
+#include "../../include/includes.h"
+
+void    make_cmd(t_glob *shell_kai)
+{
+    char    **av;
+    int     i;
+
+    i = 0;
+    av = pars(shell_kai);
+    if (!NULL)
+    {
+        
+    }
+}
