639eceea444e5bd18d3099bf2067f9a77f4f821d
007MJ <k.mijiza@gmail.com>
2024-03-08 16:14:47 -0500

    lire commentaire

--- bankai/parsing/expan_pars/remove_first_quote.c
+++ bankai/parsing/expan_pars/remove_first_quote.c
@@ -6,7 +6,7 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:38:49 by mnshimiy          #+#    #+#             */
-/*   Updated: 2024/03/08 15:20:08 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2024/03/08 16:10:28 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,39 @@ char quote_on_plist(char *s)
     }
     return (0);
 }
+int is_sign_quoted(char *s)
+{
+    (void)s;
+    return (0);
+}
+char *join_word(char **tab)
+{
+    int     i;
+    char    *s;
+    i = 0;
+    s = NULL;
+    while (tab[i + 1] != NULL)
+    {
+        printf("join -- %s --\n", s);
+        s = ft_strjoin(tab[i], tab[i + 1]);
+        i++;
+    }
+    return (s);
+}
 int remove_first_quote(char *s, t_cmd **cmds)
 {
     (void)cmds;
     char  **tab;
+    char   *cmd;
     printf("-- %c --\n", quote_on_plist(s));
     tab = ft_split(s, quote_on_plist(s));
     printf("split...\n");
     for (int i = 0; tab[i] != NULL; i++)
     {
         printf("split -- %s -- \n", tab[i]);
-        
     }
     printf("split end\n");
-    
+    cmd = join_word(tab);
+    printf("-- %s cmd --\n", cmd);
     return (1);
 }
--- bankai/parsing/expan_pars/trie_cmds.c
+++ bankai/parsing/expan_pars/trie_cmds.c
@@ -6,16 +6,38 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 15:56:21 by mnshimiy          #+#    #+#             */
-/*   Updated: 2024/03/08 14:59:19 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2024/03/08 16:14:16 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/includes.h"
 
+int is_quote(char *input)
+{
+    int one_q;
+    int two_q;
+    int i;
+
+    i = 0;
+    one_q = 0;
+    two_q = 0;
+    while (input[i] && input[i] != '\0')
+    {
+        if (input[i] == '\'')
+            one_q++;
+        if (input[i] == '\"')
+            two_q++;
+        i++;
+    }
+    if (one_q % 2 == 0 || two_q  % 2 == 0)
+        return (1);
+    return (-1);
+}
 
 int trie_cmd(t_cmd **cmds, char *av)
 {
-    if (check_quote(av) != -1)
+    // tu dois faire en sorte   que seulement ceux qui on des quote sont prise en charge par la function
+    if (is_quote(av) == 1)
         remove_first_quote(av, cmds);
     (void)cmds;
     // tu remove les first quote du mots et tu colle le mots si nécessaire
