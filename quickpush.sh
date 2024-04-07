224b7f92b90d3bc074894a578f470d6971fe4a40
007MJ <k.mijiza@gmail.com>
2024-04-07 10:05:39 -0400

    test

--- quickpush.sh
+++ quickpush.sh
@@ -1,14 +1,19 @@
-!/bin/bash
+#!/bin/bash
+
+
+# ======================== #
+#     get user input       #
+#========================= #
 
 echo "What is the message of commit:"
-
 read message
 datep=$(date)
 
-commitMessage=$($message + $datep)
+# ======================== #
+#     command git          #
+#========================= #
 
-git add .
 
-git commit -m commitMessage
-
+git add *
+git commit -m $message + $datep
 git push
